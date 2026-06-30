d5a0dc300f48a6e55df2882dd7bcb10d12840a83
Akshat Vyas <akshatatworkmail@gmail.com>
2026-06-30 06:14:31 +0530

    Fix Firebase Admin for Netlify

--- netlify/functions/firebaseAdmin.js
+++ netlify/functions/firebaseAdmin.js
@@ -1,9 +1,30 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : null;
+
+if (!projectId) {
+  throw new Error("FIREBASE_PROJECT_ID is missing");
+}
+
+if (!clientEmail) {
+  throw new Error("FIREBASE_CLIENT_EMAIL is missing");
+}
+
+if (!privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing");
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey,
+    }),
   });
 
   console.log("Firebase Admin initialized");
