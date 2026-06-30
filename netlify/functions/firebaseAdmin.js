e1fea61b6e5454a50c54d36c44f398744e4d82bf
Akshat Vyas <akshatatworkmail@gmail.com>
2026-06-30 06:12:29 +0530

    Add Firebase push notification backend

--- netlify/functions/firebaseAdmin.js
+++ netlify/functions/firebaseAdmin.js
@@ -1,15 +1,12 @@
 const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-        : undefined,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
+
+  console.log("Firebase Admin initialized");
 }
 
 const db = admin.firestore();
